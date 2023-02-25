45c5184f67bde530e20f0f13215f2eae7caf6587
PH <paulo_phs01@hotmail.com>
2023-02-24 21:22:58 -0300

    mudanças js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
-alert("Eae meu fi kkkkkkkk!")
-const a = 10
-const b = 20
+// alert("Eae meu fi kkkkkkkk!")
+const a = Number(prompt("Digite o valor de A: "))
+const b = Number(prompt("Digite o valor de B: "))
 
-let resultado = a+b
-resultado = a*b
 
+const lista = [a,b]
+
+let resultado = a + b
 console.log(resultado)
+
+resultado = a-b
+console.log(resultado)
+
+document.write(resultado)
